098dfc314bd28fe9e4a1e20bcc900ef10a428fb2
starxing <starxing0321@163.com>
2026-02-14 21:14:26 +0800

    1

--- web/lib/site-url.js
+++ web/lib/site-url.js
@@ -1,4 +1,4 @@
-export const DEFAULT_SITE_URL = 'https://prod-divination.vercel.app';
+export const DEFAULT_SITE_URL = 'https://divination-eight.vercel.app';
 
 export function resolveSiteUrl() {
   return process.env.NEXT_PUBLIC_SITE_URL || DEFAULT_SITE_URL;
